85ca5c1229ccd3ff171fede865268dd59cd4e5f8
Kevin Kelly <kvinklly@gmail.com>
2019-10-10 09:43:49 -0500

    add test for setFetchedUsersDate

--- tests/unit/store/modules/users.spec.js
+++ tests/unit/store/modules/users.spec.js
@@ -1,7 +1,11 @@
 import user_store from "@/store/modules/users";
-const { state, getters, mutations } = user_store;
+const { state, getters, mutations, actions } = user_store;
 import { expect } from "chai";
-const testState = { users: [1, 2, 3] };
+const initialState = {
+  users: [1, 2, 3],
+  fetchedUsersDate: null
+};
+
 const fetchedUsers = ["a", "b", "c"];
 
 describe("users store", () => {
@@ -14,16 +18,30 @@ describe("users store", () => {
   describe("getters", () => {
     describe("users", () => {
       it("returns all users from the state", () => {
-        expect(getters.users(testState)).to.equal(testState.users);
+        expect(getters.users(initialState)).to.equal(initialState.users);
       });
     });
   });
 
   describe("mutations", () => {
+    let testState;
+
+    beforeEach(() => {
+      testState = { ...initialState };
+    });
+
     describe("setUsers", () => {
       it("should set state.users to users", () => {
         mutations.setUsers(testState, fetchedUsers);
         expect(testState.users).to.equal(fetchedUsers);
+      });
+    });
+
+    describe("setFetchedUsersDate", () => {
+      it("should set state.fetchedUsersDate", () => {
+        const date = new Date();
+        mutations.setFetchedUsersDate(testState, date);
+        expect(testState.fetchedUsersDate).to.equal(date);
       });
     });
   });
